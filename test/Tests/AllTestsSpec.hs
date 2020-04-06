a9190efcaa5dcf444371b7aa6220816617a093d5
Mahler, Thomas <thomas.mahler@ista.com>
2020-04-06 09:24:53 +0200

    fix test case

--- test/Tests/AllTestsSpec.hs
+++ test/Tests/AllTestsSpec.hs
@@ -15,7 +15,6 @@ spec =
     it "can handle user defined controll structures" $
       cond [(False, viciousCircle), 
             (True, 3), 
-            (True, 5), 
-            (False, viciousCircle)] `shouldBe` [3,5]
+            (False, viciousCircle)] `shouldBe` 3
     it "can build trees" $
       show (Node (Leaf 1) (Node (Leaf 2) (Leaf 3))) `shouldBe` "Node (Leaf 1) (Node (Leaf 2) (Leaf 3))"
